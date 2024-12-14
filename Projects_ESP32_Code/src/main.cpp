9b36ed6ee1c43b00e4f72b49ad79734caef8f597
e-sarvey <elijah.sarvey@tufts.edu>
2024-12-14 16:04:03 -0500

    our attempt at an observer

--- Projects_ESP32_Code/src/main.cpp
+++ Projects_ESP32_Code/src/main.cpp
@@ -4,190 +4,127 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Variables to adjust
-float desired_angle_deg = 0.0;
-float Kp = 1.6e-3;
-float Kd = 0.001;
-float Ki = 0.0001;
+// physical parameters
+const float m = 0.00217;
+const float g = 9.81;
+const float l = 0.0508;
+const float J = 1.8797e-06;
+const float b = 0.007;
+const float stall_torque = 0.004*(9/5);
+const float counts_per_rot = 400;
 
-float pwm_offset = 0.0; // the default PWM to reach 45 degrees
-float max_pwm = 255;
-float corner_freq = 10;
+// Setup PID constants
+const float desired_angle_deg = 0.0;
+const float Kp = 1.6e-3;
+const float Kd = 0.001;
+const float Ki = 0.0001;
 
-// Global variables
+// observer constants
+const float L1 = -3700;
+const float L2 = 13778607.77;
+const float mgl_over_J = m*g*l/J;
+const float b_over_J = b/J;
+
+const float pwm_offset = 0.0; // the default PWM to reach 45 degrees
+const float max_pwm = 255;
+const float corner_freq = 10; // for filter
+
+// Pin definitions
+const int Bin1_pin = 22;  // motor pin 1
+const int Bin2_pin = 23;  // motor pin 2
+const int zeroButtonPin = 0; // Button pin for zeroing (BOOT button is GPIO 0)
+
+// Encoder Varaibles
 ESP32Encoder encoder;
-unsigned long myTime;
-unsigned long myTime2;
-int8_t dutyCycle = 0;
 char myData[50];
 long int position = 0;
-long int reference = 0;
-long int gain = 1;
 
-// Motor pins
-const int Bin1_pin = 22;  
-const int Bin2_pin = 23;   
-// Button pin for zeroing (BOOT button is GPIO 0)
-const int zeroButtonPin = 0;
+int8_t dutyCycle = 0;
 
-// System parameters
-float counts_per_rot = 400;
-float stallTorque = 0.004*(9/5);
-float torque = 0;
-float angle_rad = 0;
-float Ts = 0; 
-float angle_rad_prev = 0;
-float tt_sec = 0;
-float tt_sec_last = 0.0;
+// initialize observer variables
+float Ts = 0.01;
+float x1_hat = 0.0;
+float x2_hat = 0.0;
+float x1_hat_dot = 0.0;
+float x2_hat_dot = 0.0;
+float angle_rad = 0.0;
+float theta = 0.0;
 float theta_dot = 0.0;
-float error = 0;
-// PID variables for angular position
-float integral = 0;
-float previous_error = 0;
-float desired_angle_rad = desired_angle_deg *(PI/180);
+float t_curr = 0.0;
+float t_prev = 0.0;
+float x1_hat_old = 0.0;
+float x2_hat_old = 0.0;
 
-// Function to parse and update PID gains
-// void parseAndUpdateGains(const char* input) {
-//   float parsedKp, parsedKd, parsedKi;
-  
-//   if (sscanf(input, "[%f,%f,%f]", &parsedKp, &parsedKd, &parsedKi) == 3) {
-//     Kp = parsedKp;
-//     Kd = parsedKd;
-//     Ki = parsedKi;
-    
-//     Serial.println("Updated PID Gains:");
-//     Serial.print("Kp = "); Serial.println(Kp);
-//     Serial.print("Kd = "); Serial.println(Kd);
-//     Serial.print("Ki = "); Serial.println(Ki);
-//   } else {
-//     Serial.println("Invalid input format for PID gains. Please use [Kp,Kd,Ki]");
-//   }
-// }
-
-// Function to handle serial communication, including PID gains input
-long int SerialComm() {
-
+void SerialComm() {
   // Serial Plotter Mode with new syntax
   Serial.print(">");
   Serial.print("position:"); Serial.print(position);
-  Serial.print(",angle:"); Serial.print(angle_rad * (180 / 3.1416));  // Convert radians to degrees
+  Serial.print(",raw_angle:"); Serial.print(position*360/counts_per_rot);
+  Serial.print(",obsv_angle:"); Serial.print(theta * (180 / 3.1416));  // Convert radians to degrees
   Serial.print(",velocity:"); Serial.print(theta_dot * (180 / 3.1416));  // Convert rad/s to deg/s
   // Serial.print(",torque:"); Serial.print(torque);
-  Serial.print(",pwm:"); Serial.print(dutyCycle);
-  //Serial.print(",Sample Time:"); Serial.print(Ts);
+  //Serial.print(",pwm:"); Serial.print(dutyCycle);
+  Serial.print(",Sample_Time:"); Serial.print(Ts);
   Serial.print(",Scaling:"); Serial.print(60);
   //Serial.print(",Error:"); Serial.print(error);
   Serial.println("\r");
-
-  return dutyCycle;
-}
-
-// Function to control motor based on dutyCycle
-void controlMotor(long int dutyCycle) {
-  if (dutyCycle >= 0) {
-    analogWrite(Bin1_pin, dutyCycle);
-    analogWrite(Bin2_pin, 0);
-  } else {
-    analogWrite(Bin1_pin, 0);
-    analogWrite(Bin2_pin, abs(dutyCycle));
-  }
-}
-
-// Function to handle encoder readings, calculate filtered angle, and compute angular velocity
-void getAngleAndVelocityFilt() {
-  position = (int32_t)encoder.getCount();
-  float angle_rad_raw = 2.0 * 3.1416 * position / counts_per_rot;
-  tt_sec = (micros()) / 1e6;
-  Ts = tt_sec - tt_sec_last;
-
-  if (Ts > 0) {
-    angle_rad = (angle_rad_prev + Ts * corner_freq * angle_rad_raw) / (1 + Ts * corner_freq);
-    theta_dot = (angle_rad - angle_rad_prev) / Ts;
-    angle_rad_prev = angle_rad;
-  } else if (Ts == 0 ){
-    Serial.println("CHANGE IN TIME IS 0. BAD!!");
-  }
-  tt_sec_last = tt_sec;
-}
-
-// Function to estimate torque based on PWM and stall torque
-void estimateTorque() {
-  torque = stallTorque * (dutyCycle / 255.0);
-}
+}\
 
 // Function to zero the encoder when the button is pressed
-void checkZeroButton() {
+void zeroEncoderButton() {
   if (digitalRead(zeroButtonPin) == LOW) {
     encoder.setCount(0);
-    angle_rad = 0;
+    theta = 0;
     Serial.println("Encoder zeroed!");
     delay(500);
   }
 }
 
-// PID control function to compute the desired torque based on angular error
-float PIDControlForAngle(float desired_angle, float actual_angle) {
-  
-  error = desired_angle - actual_angle;
-
-  float Pout = Kp * error;
-  integral += error * Ts;
-  float Iout = Ki * integral;
-  float derivative = theta_dot;
-  float Dout = Kd * derivative;
-
-  float output_torque = Pout + Iout + Dout;
-  previous_error = error;
-
-  return output_torque;
-}
-
-// Function to compute PWM from torque
-long int computePWMFromTorque(float torque) {
-  long int pwm_output = ((torque / stallTorque) * 255.0) + pwm_offset;
-
-  return pwm_output;
-}
 
 void setup() {
   Serial.begin(115200, SERIAL_8N1); 
 
+  // setup motor pins
   pinMode(Bin1_pin, OUTPUT);
-pinMode(Bin2_pin, OUTPUT);
-  pinMode(zeroButtonPin, INPUT_PULLUP);
+  pinMode(Bin2_pin, OUTPUT);
+  pinMode(zeroButtonPin, INPUT_PULLUP); // setup reset button
 
-  ESP32Encoder::useInternalWeakPullResistors = puType::up;
+  ESP32Encoder::useInternalWeakPullResistors = puType::up; 
   encoder.attachFullQuad(19, 18);
   encoder.setCount(0);
-
-  Serial.println("Encoder Start = " + String((int32_t)encoder.getCount()));
-
 
 }
 
 void loop() {
-  checkZeroButton();
-  getAngleAndVelocityFilt();
+  zeroEncoderButton();
+  position = (int32_t)encoder.getCount();
+  angle_rad = 2.0 * 3.1416 * position / counts_per_rot;
   
-  float desired_torque = PIDControlForAngle(desired_angle_rad, angle_rad);
-  
-  dutyCycle = computePWMFromTorque(desired_torque);
-
-  // Serial.print("a ang:"); Serial.print(actual_angle);
-  // Serial.print("d torque (x1000)::"); Serial.print(desired_torque * 1e3);
-  // Serial.print(" pwm:"); Serial.print(dutyCycle);
-  // Serial.println("\r");
-
-  // Serial.print("position:"); Serial.print(position);
-  // Serial.print(",angle:"); Serial.print(angle_rad * (180 / 3.1416));  // Convert radians to degrees
-  // Serial.print(",velocity:"); Serial.print(theta_dot * (180 / 3.1416));  // Convert rad/s to deg/s
-  // Serial.print(",torque:"); Serial.print(torque);
-  // Serial.print(",pwm:"); Serial.print(dutyCycle);
-  // Serial.print("Sample Time:"); Serial.print(Ts);
-  // Serial.println("\r");
+  t_curr = millis();
+  t_curr = t_curr/1000;
+  Ts = t_curr - t_prev;
 
 
-  controlMotor(dutyCycle);
+  x1_hat = (x2_hat - L1 * angle_rad + x1_hat_old / Ts) / (1 / Ts + L1);
+  x2_hat = (x2_hat_old / Ts + L2 * angle_rad - L2 * x1_hat) / (1 / Ts + b_over_J);
+
+  x1_hat_dot = (x1_hat - x1_hat_old) / Ts;
+
+  x1_hat_old = x1_hat;
+  x2_hat_old = x2_hat;
+
+
+  // x1_hat_dot = -L1*x1_hat + x2_hat + L1*angle_rad;
+  // x2_hat_dot = -(L2 + mgl_over_J)*x1_hat - b_over_J*x2_hat + L2*angle_rad;
+
+  theta = x1_hat;
+  theta_dot = x1_hat_dot;
+
+  // x1_hat += x1_hat_dot*Ts; // += same as x[k]_prev +Tsf(x[k])
+  // x2_hat += x2_hat_dot*Ts;
+
+  t_prev = t_curr;
+
   SerialComm();
-  delayMicroseconds(100);
+  delayMicroseconds(1000);
 }
